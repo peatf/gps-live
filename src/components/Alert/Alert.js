e4f03e0f8b994bab3c3677bac58ae010930b46a5
peatf <peathefeary@gmail.com>
2025-01-08 01:01:46 -0800

    Update Alert.js

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+export const Alert = ({ children, className }) => (
+  <div className={`p-4 rounded border ${className || ""}`}>
+    {children}
+  </div>
+);
+
+export const AlertDescription = ({ children }) => (
+  <p className="text-sm">{children}</p>
+);
