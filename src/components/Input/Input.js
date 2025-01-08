f15e6452d15a445783e68c0fa538d89b5fd4a7dc
peatf <peathefeary@gmail.com>
2025-01-07 21:26:38 -0800

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,1 +1,11 @@
+import React from "react";
+
+export const Input = ({ type, value, onChange, className }) => (
+  <input
+    type={type}
+    value={value}
+    onChange={onChange}
+    className={`border rounded px-3 py-2 w-full ${className || ""}`}
+  />
+);
 
