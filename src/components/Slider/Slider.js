d2c8f535b9903005470947a90bb6b7b33ea8cbc4
peatf <peathefeary@gmail.com>
2025-01-09 13:15:28 -0800

    Update Slider.js

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,13 +1,13 @@
-// src/components/Slider/Slider.js
+/ src/components/Slider/Slider.js
 import React from "react";
-import { cn } from "../../utils/cn";  // Fixed import path
+import { cn } from "../../utils/cn";
 
 export const Slider = ({ value, min, max, step, onValueChange, className }) => {
   return (
     <div className="relative flex w-full touch-none select-none items-center">
-      <div className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+      <div className="relative h-2 w-full grow overflow-hidden rounded-full bg-stone/30">
         <div
-          className="absolute h-full bg-primary-600 transition-all"
+          className="absolute h-full bg-cosmic transition-all duration-200"
           style={{
             width: `${((value[0] - min) / (max - min)) * 100}%`
           }}
@@ -26,20 +26,24 @@ export const Slider = ({ value, min, max, step, onValueChange, className }) => {
           "[&::-webkit-slider-thumb]:w-4",
           "[&::-webkit-slider-thumb]:appearance-none",
           "[&::-webkit-slider-thumb]:rounded-full",
-          "[&::-webkit-slider-thumb]:bg-primary-600",
+          "[&::-webkit-slider-thumb]:bg-cosmic",
           "[&::-webkit-slider-thumb]:border-2",
           "[&::-webkit-slider-thumb]:border-white",
-          "[&::-webkit-slider-thumb]:transition-colors",
-          "[&::-webkit-slider-thumb]:hover:bg-primary-700",
+          "[&::-webkit-slider-thumb]:transition-all",
+          "[&::-webkit-slider-thumb]:duration-200",
+          "[&::-webkit-slider-thumb]:hover:bg-cosmic-light",
+          "[&::-webkit-slider-thumb]:hover:scale-110",
           "[&::-moz-range-thumb]:h-4",
           "[&::-moz-range-thumb]:w-4",
           "[&::-moz-range-thumb]:appearance-none",
           "[&::-moz-range-thumb]:rounded-full",
-          "[&::-moz-range-thumb]:bg-primary-600",
+          "[&::-moz-range-thumb]:bg-cosmic",
           "[&::-moz-range-thumb]:border-2",
           "[&::-moz-range-thumb]:border-white",
-          "[&::-moz-range-thumb]:transition-colors",
-          "[&::-moz-range-thumb]:hover:bg-primary-700",
+          "[&::-moz-range-thumb]:transition-all",
+          "[&::-moz-range-thumb]:duration-200",
+          "[&::-moz-range-thumb]:hover:bg-cosmic-light",
+          "[&::-moz-range-thumb]:hover:scale-110",
           className
         )}
       />
