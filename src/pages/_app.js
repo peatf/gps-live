ceae9e67c6dcd5184f042074fe08d2f222e8672e
peatf <peathefeary@gmail.com>
2025-01-09 13:39:07 -0800

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
+// src/pages/_app.js
 import '../styles/globals.css';
+import ResponsiveContainer from '../components/ResponsiveContainer';
+import { useEffect } from 'react';
+import { setupIframeResizing } from '../utils/iframeUtils';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Set up iframe resizing
+    const cleanup = setupIframeResizing();
+    return cleanup;
+  }, []);
+
+  return (
+    <ResponsiveContainer>
+      <Component {...pageProps} />
+    </ResponsiveContainer>
+  );
 }
 
 export default MyApp;
