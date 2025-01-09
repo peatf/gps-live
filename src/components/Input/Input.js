9bfd51badc6b7558acb214af85fceba411c5e32b
peatf <peathefeary@gmail.com>
2025-01-09 13:18:36 -0800

    Update Input.js

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { cn } from "../../utils/cn";
 
-export const Input = ({ type, value, onChange, className }) => (
+export const Input = React.forwardRef(({ className, ...props }, ref) => (
   <input
-    type={type}
-    value={value}
-    onChange={onChange}
-    className={`border rounded px-3 py-2 w-full ${className || ""}`}
+    ref={ref}
+    className={cn(
+      "flex h-10 w-full rounded-md border border-stone bg-cloud px-3 py-2",
+      "text-sm text-earth placeholder:text-dove",
+      "transition-all duration-200",
+      "focus:outline-none focus:border-cosmic focus:ring-1 focus:ring-cosmic",
+      "hover:border-cosmic-light",
+      className
+    )}
+    {...props}
   />
-);
-
+));
