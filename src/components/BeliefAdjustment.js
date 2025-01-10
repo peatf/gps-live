1fb109bfe58e664d54e81d6c349c7c9115ccf368
peatf <peathefeary@gmail.com>
2025-01-10 10:17:41 -0800

    Update BeliefAdjustment.js

--- src/components/BeliefAdjustment.js
+++ src/components/BeliefAdjustment.js
@@ -1,5 +1,5 @@
+// src/components/BeliefAdjustment.js
 import React, { useState, useEffect } from 'react';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardContent } from './Card/Card';
 import { Button } from './Button/Button';
 import { Alert, AlertDescription } from './Alert/Alert';
