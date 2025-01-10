7a06e01470191dfa373ba1e391f25dd9ff65f3b3
peatf <peathefeary@gmail.com>
2025-01-09 19:35:56 -0800

    Update ResponsiveContainer.js

--- src/components/ResponsiveContainer.js
+++ src/components/ResponsiveContainer.js
@@ -1,9 +1,8 @@
-// src/components/ResponsiveContainer.js
 import React from 'react';
 
 const ResponsiveContainer = ({ children }) => {
   return (
-    <div className="w-full">
+    <div className="w-full h-full scrollable">
       <div className="w-full px-4 py-8">{children}</div>
     </div>
   );
