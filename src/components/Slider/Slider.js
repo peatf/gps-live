6ea8381aa296399c3eb0251b63a3eb7faaf56aa2
peatf <peathefeary@gmail.com>
2025-01-10 06:34:09 -0800

    Update Slider.js

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,9 +4,9 @@ import { cn } from "../../utils/cn";
 export const Slider = ({ value, min, max, step, onValueChange, className }) => (
   <div className={cn("relative flex w-full items-center", className)}>
     {/* Track */}
-    <div className="relative w-full h-3 rounded-full bg-gray-200 shadow-inner">
+    <div className="relative w-full h-4 rounded-full bg-gray-200 shadow-inner">
       <div
-        className="absolute h-full bg-gradient-to-r from-blue-300 to-blue-500 rounded-full"
+        className="absolute h-full rounded-full bg-gradient-to-r from-blue-300 to-blue-500"
         style={{
           width: `${((value[0] - min) / (max - min)) * 100}%`,
         }}
@@ -22,21 +22,18 @@ export const Slider = ({ value, min, max, step, onValueChange, className }) => (
       step={step}
       onChange={(e) => onValueChange([parseInt(e.target.value, 10)])}
       className={cn(
-        "absolute w-full h-3 opacity-0", // Invisible input for interaction
-        "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-6 [&::-webkit-slider-thumb]:h-6",
+        "absolute w-full h-4 opacity-0 cursor-pointer",
+        "[&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-8 [&::-webkit-slider-thumb]:h-8",
         "[&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-gradient-to-br",
-        "[&::-webkit-slider-thumb]:from-silver-300 [&::-webkit-slider-thumb]:to-silver-500",
+        "[&::-webkit-slider-thumb]:from-white [&::-webkit-slider-thumb]:to-blue-400",
         "[&::-webkit-slider-thumb]:shadow-md [&::-webkit-slider-thumb]:hover:shadow-lg",
         "[&::-webkit-slider-thumb]:transition-all [&::-webkit-slider-thumb]:duration-300",
-        "[&::-webkit-slider-thumb]:border [&::-webkit-slider-thumb]:border-white",
-        "[&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-6 [&::-moz-range-thumb]:h-6",
+        "[&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-8 [&::-moz-range-thumb]:h-8",
         "[&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-gradient-to-br",
-        "[&::-moz-range-thumb]:from-silver-300 [&::-moz-range-thumb]:to-silver-500",
+        "[&::-moz-range-thumb]:from-white [&::-moz-range-thumb]:to-blue-400",
         "[&::-moz-range-thumb]:shadow-md [&::-moz-range-thumb]:hover:shadow-lg",
-        "[&::-moz-range-thumb]:transition-all [&::-moz-range-thumb]:duration-300",
-        "[&::-moz-range-thumb]:border [&::-moz-range-thumb]:border-white"
+        "[&::-moz-range-thumb]:transition-all [&::-moz-range-thumb]:duration-300"
       )}
     />
   </div>
 );
-
