907240bc8b65229dcf59dce80003a7b6f3cbc400
peatf <peathefeary@gmail.com>
2025-01-09 20:51:32 -0800

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,7 +8,7 @@ export const Card = ({ children, className }) => (
       className
     )}
     style={{
-      backgroundColor: "rgba(255, 255, 255, 0.1)", // Ensure translucency
+      backgroundColor: "rgba(255, 255, 255, 0.01)", // Ensure translucency
       backdropFilter: "blur(8px)", // Add blur effect
       border: "1px solid rgba(0, 0, 0, 0.1)", // Subtle border for contrast
     }}
