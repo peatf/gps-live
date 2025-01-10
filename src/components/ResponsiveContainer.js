101dbd00f4eea4ff0104ef11334f46cb54762e16
peatf <peathefeary@gmail.com>
2025-01-09 16:44:26 -0800

    Update ResponsiveContainer.js

--- src/components/ResponsiveContainer.js
+++ src/components/ResponsiveContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ResponsiveContainer = ({ children }) => {
   return (
-    <div className="w-full">
+    <div className="w-full scale-in">
       <div className="max-w-screen-xl mx-auto px-4 py-8">{children}</div>
     </div>
   );
